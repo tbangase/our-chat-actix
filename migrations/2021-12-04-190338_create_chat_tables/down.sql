e7a976c207c9c5e8e7ce52173cd8ebd7f2033e15
TosHiki <ichi.toshi217@gmail.com>
2021-12-05 16:30:31 +0900

    fix: Remove volumes, update: Use Data base for Room Create.

--- migrations/2021-12-04-190338_create_chat_tables/down.sql
+++
@@ -1,8 +0,0 @@
--- This file should undo anything in `up.sql`
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS messages;
-DROP TABLE IF EXISTS subscribes;
-SET FOREIGN_KEY_CHECKS = 1;
-
--- src/models/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod users;
-pub mod rooms;
-pub mod messages;
-pub mod subscribes;
